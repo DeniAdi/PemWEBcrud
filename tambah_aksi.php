a3461e100f9dec7e69c34f96344da0dd0faa0fa9
DeniAdi <139104177+DeniAdi@users.noreply.github.com>
2023-07-10 13:35:32 +0700

    Add files via upload

---
+++ tambah_aksi.php
@@ -0,0 +1,27 @@
+<?php
+include ("koneksi.php");
+
+//jika tombol submit diklik
+if (isset($_POST['submit'])) {
+
+//ambil data dari form
+$nim = $_post['nim'];
+$nama_mahasiswa = $_POST['nama_mahasiswa'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$program_studi = $_POST['program_studi'];
+$keahlian = implode(",", $_POST['keahlian']);
+
+//membuat query
+$sql = "INSERT into mahasiswa(nim,nama_mahasiswa,jenis_kelamin,program_studi,keahlian) VALUES ('$nim','$nama_mahasiswa','$jenis_kelamin','$program_studi','$keahlian')";
+$query = mysqli_query($koneksi, $sql);
+
+
+if ($query) {
+    header('Location: index.php');
+} else {
+    echo "Gagal menambahkan data mahasiswa <a href=index.php>Halaman Index</a>";
+}
+} else {
+    echo "Anda tidak mempunyai akses <a href=index.php>Halaman Index</a>";
+}
+?>
